bf54edb9579de6ce3482017c73c711d9794a6add
alinakassym <alinakassym@gmail.com>
2025-09-10 13:05:49 +0500

    feat(home): add Carousel

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { ScrollView, View } from "react-native";
 import { Text } from "@/components/ui/text";
+import Carousel from "@/components/Carousel";
+import { banners } from "@/mocks/banners";
 
 const HomeScreen = () => {
   return (
     <ScrollView className="flex-1 bg-white">
+      <Carousel items={banners} />
       <View className="px-6 py-8">
         <Text className="text-2xl font-bold mb-6">Главная</Text>
 
